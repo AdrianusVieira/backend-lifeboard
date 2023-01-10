08b495ab455fddc09e2dffa8314d5184eb86400a
Adrianus Vieira <adrianusvieira@cpejr.com.br>
2023-01-10 19:09:05 -0300

    feat: troca de foto funcional

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
-
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 3333;
 
 const app = express();
-
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    parameterLimit: 100000,
+    limit: "500mb",
+  })
+);
+app.use(
+  bodyParser.json({ extended: true, parameterLimit: 100000, limit: "500mb" })
+);
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
-
-app.listen(port,()=>{
-    console.log('Server listening on port:' + port)
-})
+app.listen(port, () => {
+  console.log("Server listening on port:" + port);
+});
